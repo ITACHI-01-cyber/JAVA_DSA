21ca398e4682ae58ab9831f51701e028c7295674
ITACHI-01-cyber <bvivek514@gmail.com>
2026-05-30 18:53:59 +0530

    Day 01 Restar

--- Stack/stackRev.java
+++ Stack/stackRev.java
@@ -65,59 +65,61 @@
 
 import java.net.StandardSocketOptions;
 
-class stackRev{
-        int top;
-        int[] StackArray;
-        int maxSize;
+// class stackRev{
+//         int top;
+//         int[] StackArray;
+//         int maxSize;
 
-        stackRev(int size){
-            maxSize = size;
-            StackArray = new int[maxSize];
-            top = -1;
-        }
+//         stackRev(int size){
+//             maxSize = size;
+//             StackArray = new int[maxSize];
+//             top = -1;
+//         }
 
-        void push(int val){
-            if (isFull()){
-                System.out.println("Stack Overflow !!!" + val);
-            }
-            StackArray[++top] = val;
-            System.out.println(val + "Pushed in Stack");
-        }
+//         void push(int val){
+//             if (isFull()){
+//                 System.out.println("Stack Overflow !!!" + val);
+//             }
+//             StackArray[++top] = val;
+//             System.out.println(val + "Pushed in Stack");
+//         }
 
-        int pop(){
-            if (isEmpty()){
-                System.out.println("Stack UnderFlow !!" );
-                return -1;
-            }
-            return StackArray[top--];
-    }
-    int peek(){
-        if (isEmpty()){
-            System.out.println("Stack UnderFlow !!" );
-            return -1;
-        }
-        return StackArray[top];
-    }
-    boolean isFull(){
-            return (top == maxSize - 1);
-    }
-    boolean isEmpty(){
-            return (top == maxSize);
-    }
-    boolean Print(){
-            for (int i = top; i>= 0;i--){
-                System.out.print("" + StackArray[i] + " ");
-            }
-            System.out.println();
-            return true;
-    }
-    public static void main(String[] args) {
-        stackRev obj = new stackRev(7);
+//         int pop(){
+//             if (isEmpty()){
+//                 System.out.println("Stack UnderFlow !!" );
+//                 return -1;
+//             }
+//             return StackArray[top--];
+//     }
+//     int peek(){
+//         if (isEmpty()){
+//             System.out.println("Stack UnderFlow !!" );
+//             return -1;
+//         }
+//         return StackArray[top];
+//     }
+//     boolean isFull(){
+//             return (top == maxSize - 1);
+//     }
+//     boolean isEmpty(){
+//             return (top == maxSize);
+//     }
+//     boolean Print(){
+//             for (int i = top; i>= 0;i--){
+//                 System.out.print("" + StackArray[i] + " ");
+//             }
+//             System.out.println();
+//             return true;
+//     }
+//     public static void main(String[] args) {
+//         stackRev obj = new stackRev(7);
 
-        obj.push(10);obj.push(20);obj.push(30);obj.push(40);obj.push(60);obj.push(70);obj.push(80);
-        obj.Print();
-        System.out.println("Stack POP +> "+ obj.pop());
-        System.out.println("Stack After pop :"+ obj.Print());
-        System.out.println("Stack PEEK +> "+ obj.peek());
-    }
-    }
+//         obj.push(10);obj.push(20);obj.push(30);obj.push(40);obj.push(60);obj.push(70);obj.push(80);
+//         obj.Print();
+//         System.out.println("Stack POP +> "+ obj.pop());
+//         System.out.println("Stack After pop :"+ obj.Print());
+//         System.out.println("Stack PEEK +> "+ obj.peek());
+//     }
+//     }
+
+// 
